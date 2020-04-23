2054029ea96f094ee29f5a6f494322a10517ba95
Juliano Zanella <juliano.zanella.live@gmail.com>
2020-04-23 17:18:19 -0300

    36 - Exercicio 2

--- Livro/Livro.cpp
+++ Livro/Livro.cpp
@@ -7,14 +7,28 @@
 #include <locale.h>
 #include <math.h>
 
+struct semaforo {
+	char cor[50];
+	int id;
+};
+semaforo a = { "vermelha", 1 };
+semaforo b = { "amarela", 2 };
+semaforo c = { "verde", 3 };
+semaforo* ptr;
+
+
+
 int main()
 {
 	setlocale(LC_ALL, "Portuguese");
 
-	int* ptr;
-	ptr = (int*)malloc(sizeof(int));
-	*ptr = 42;
-	printf("Endereço: %p, valor: %d\n", ptr, *ptr);
+	ptr = &a;
+
+	printf("Semáforo: %d, cor: %s\n", (*ptr).id, (*ptr).cor);
+	ptr++;
+	printf("Semáforo: %d, cor: %s\n", (*ptr).id, (*ptr).cor);
+	ptr++;
+	printf("Semáforo: %d, cor: %s\n", (*ptr).id, (*ptr).cor);
 
 	system("Pause");
 	return 0;
