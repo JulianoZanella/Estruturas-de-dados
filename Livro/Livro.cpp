90d1fdf78156506f182b5a0b53c8e578822e8506
Juliano Zanella <juliano.zanella.live@gmail.com>
2020-04-23 16:51:27 -0300

    Add project files.

---
+++ Livro/Livro.cpp
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS //Para remover avisos do scanf no Visual Studio (Retirar no Dev-C++)
+/*
+* Author: Juliano Zanella
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+    printf("Hello Estruturas de Dados I");
+}
