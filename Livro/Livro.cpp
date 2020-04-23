d7d17ab29c27ed7c0d78853b7652691eea9a0841
Juliano Zanella <juliano.zanella.live@gmail.com>
2020-04-23 16:57:27 -0300

    8 - Ponteiros

--- Livro/Livro.cpp
+++ Livro/Livro.cpp
@@ -6,8 +6,26 @@
 #include <stdlib.h>
 #include <locale.h>
 
+int xi;
+int* ptr_xi;
+
+void imprimir();
+
 int main()
 {
-    setlocale(LC_ALL, "Portuguese");
-    printf("Hello Estruturas de Dados I");
+	setlocale(LC_ALL, "Portuguese");
+
+	xi = 10;
+	ptr_xi = &xi;
+	imprimir();
+
+	return 0;
+}
+
+void imprimir() {
+	printf("Valor de xi = %d \n", xi);
+	printf("Valor de &xi = %p \n", &xi);
+	printf("Valor de ptr_xi = %p \n", ptr_xi);
+	printf("Valor de *ptr_xi = %d \n", *ptr_xi);
+	system("Pause");
 }
