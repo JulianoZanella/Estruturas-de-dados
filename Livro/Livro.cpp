6e0ef92dc9d47b0b3fdea17bc5e5bc2d74bb3987
Juliano Zanella <juliano.zanella.live@gmail.com>
2020-04-23 17:12:43 -0300

    20 - alocacao dinamica de vetor

--- Livro/Livro.cpp
+++ Livro/Livro.cpp
@@ -5,16 +5,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+#include <math.h>
 
 int main()
 {
 	setlocale(LC_ALL, "Portuguese");
 
-	int* ptr;
-	ptr = (int*)malloc(sizeof(int));
-	printf("Endereço: %p\nValor: %d\n\n", ptr, *ptr);
-	*ptr = 10;
-	printf("Endereço: %p\nValor: %d\n\n", ptr, *ptr);
+	int tamanho;
+	int* vetor;
+	printf("Escolha o tamanho do vetor: ");
+	scanf("%d", &tamanho);
+	vetor = (int*)malloc(sizeof(int) * tamanho);
+	for (int i = 0; i < tamanho; i++)
+	{
+		vetor[i] = pow(2, i);
+		printf("Posição %d: %d\n", i, vetor[i]);
+	}
 
+	system("Pause");
 	return 0;
 }
