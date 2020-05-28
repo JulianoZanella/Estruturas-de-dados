4ce2c11c1c1c8889c466d0b0fd45190a704fcf3d
Juliano Zanella <juliano.zanella.live@gmail.com>
2020-05-28 18:43:14 -0300

    121 - Busca em Largura

--- Livro/Livro.cpp
+++ Livro/Livro.cpp
@@ -21,7 +21,7 @@ int custo = 0;
 int* custos = NULL;
 
 //Prototipação
-void buscaEmProfundidade(struct str_no g[], int inicio, int alvo);
+void buscaEmLargura(struct str_no g[], int inicio, int alvo);
 void menu_mostrar(void);
 void criar_grafo();
 void adiciona_aresta_ao_grafo(str_no* grafo, int origem, int destino);
@@ -121,7 +121,7 @@ void menu_mostrar(void) {
 	printf("Opcoes:\n");
 	printf("\t 1 - Adicionar um Grafo\n");
 	printf("\t 2 - Exibir Grafo\n");
-	printf("\t 3 - Busca em Profundidade\n");
+	printf("\t 3 - Busca em Largura\n");
 	printf("\t 0 - Sair do programa\n");
 	printf("Opcao: ");
 }
@@ -138,58 +138,64 @@ void buscar() {
 	scanf("%d", &inicio);
 	printf("Informe o alvo da busca:");
 	scanf("%d", &alvo);
-	buscaEmProfundidade(grafo, inicio, alvo);
+	buscaEmLargura(grafo, inicio, alvo);
 	system("pause");
 }
 
-void buscaEmProfundidade(struct str_no g[], int inicio, int alvo) {
-	int pilha[MAXV]; //pilha
-	bool visitado[MAXV]; //nós visitados 
-	int indice = 0; //índice do topo da pilha 
-	bool achou = false; //flag de controle (não visitados) 
+void buscaEmLargura(struct str_no g[], int inicio, int alvo) {
+	int fila[MAXV];//fila
+	bool visitado[MAXV]; //nós visitados
+	int indice = 0;//controle da fila
+	bool achou = false; //flag (não visitados)
 	int corrente = inicio;
 	struct str_no* ptr;
 	int i;
-	printf("=-=-=-= Busca em Profundidade =-=-=-=\n");
-	//Marcando os nós como ‘não visitados’. 
-	for (i = 0; i < MAXV; i++) {
+	printf("=-=-=-= Busca em Largura =-=-=-=\n");
+	//Maracando os nós como 'não visitados'.
+	for (i = 0; i < MAXV; i++)
+	{
 		visitado[i] = false;
 	}
-	while (true) {
-		//Nó corrente não visitado? Marque como visitado.
-		//Empilhe o nó corrente. 
-		if (!visitado[corrente]) {
-			printf("VISITANDO: %d. \n", corrente);
-			if (corrente == alvo) {
-				printf("Alvo encontrado!\n\n\n");
-				return;
-			}
-			visitado[corrente] = true;
-			pilha[indice] = corrente;
-			indice++;
-		}
-		//Buscando por nós adjacentes, não visitados.  
-		achou = false;
-		for (ptr = g[corrente].proximo; ptr != NULL; ptr = ptr->proximo) {
-			if (!visitado[ptr->id]) {
-				achou = true;
-				break;
-			}
-		}
-		if (achou) {
-			//Atualizando o nó corrente.   
+	//Partindo do primeiro vértice.
+	printf("VISITANDO: %d.\n", corrente);
+	visitado[corrente] = true;
+	fila[indice] = corrente;
+	indice++;
+	while (true)
+	{
+		//Visitar os nós adjacentes ao vértice corrente
+		for (ptr = g[corrente].proximo; ptr != NULL; ptr = ptr->proximo)
+		{
+			//Caso o corrente ainda não tenha sido visitado:
 			corrente = ptr->id;
+			if (!visitado[corrente]) {
+				//Enfileira e marca como visitado.
+				printf("VISITANDO: %d.\n", corrente);
+				if (corrente == alvo) {
+					printf("Alvo encontrado!\n\n\n");
+					return;
+				}
+				visitado[corrente] = true;
+				fila[indice] = corrente;
+				indice++;
+			}
+
+		}
+		//Caso a fila não esteja vazia:
+		if (indice != 0) {
+			//Atualizando vértice corrente.
+			corrente = fila[0];
+			//Desenfileirando o primeiro vértice.
+			for (i = 1; i < indice + 1; i++)
+			{
+				fila[i - 1] = fila[i];
+			}
+			indice--;
 		}
 		else {
-			//Não há vértices adjacentes não visitados.  
-			//Tentando desempilhar o vértice do topo.
-			indice--;
-			if (indice == -1) {
-				//Não há mais vértices não visitados.
-				printf("Encerrando a busca. \n");
-				break;
-			}
-			corrente = pilha[indice];
+			//Não há mais vértices para visitar.
+			printf("Encerrando busca.\n");
+			break;
 		}
 	}
 	return;
